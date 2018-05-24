7a5ec25e3c03236d68e66e34d8ffeda7d419e096
Theodore Itzaris <theodoreme@gmail.com>
2018-05-24 15:57:14 +0300

    Add correct user to mongoDB connection

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   //db: 'mongodb://username:password@url:port/db',
-  db: 'mongodb://theoItzy:mongodb123!@ds133630.mlab.com:33630/estore',
+  db: 'mongodb://userTheo:katoportokali@ds133630.mlab.com:33630/estore',
   db_dev: 'mongodb://localhost:27017/test',
 };
