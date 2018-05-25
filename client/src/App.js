a6c1335519a15267e7b62b0dbbeb1a59fabfa886
Theodore Itzaris <theodoreme@gmail.com>
2018-05-25 12:10:22 +0300

    Config shows a dummy  DB connection

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const server = 'https://damp-brook-72767.herokuapp.com'
+const server = 'https://damp-brook-72767.herokuapp.com' // Heroku deployed application url
 class App extends Component {
   state = {
     response: '',
--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  //db: 'mongodb://username:password@url:port/db',
-  db: 'mongodb://userTheo:katoportokali@ds133630.mlab.com:33630/estore',
+  // if case you host db on mlab.com the connection string 
+  // should look like, mongodb://<dbuser>:<dbpassword>@ds133630.mlab.com:33630/db
+  db: 'mongodb://username:password@url:port/db',
   db_dev: 'mongodb://localhost:27017/test',
 };
