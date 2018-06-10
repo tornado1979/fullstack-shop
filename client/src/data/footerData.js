a15aed150f607723535f90965142294cc8dae935
Theodoros Itzaris <theodoreme@gmail.com>
2018-06-10 21:05:26 +0300

    client: Add dummy footer data
    - Todo: move tgese data on the state

---
+++ client/src/data/footerData.js
@@ -0,0 +1,79 @@
+import img from '../assets/img/payments.jpg'
+import facebook from '../assets/img/icon_facebook.png'
+import linkedin from '../assets/img/icon_linkedin.png'
+import youtube from '../assets/img/icon_youTube.png'
+import contact from '../assets/img/icon_contact.png'
+
+export const socialData = {
+  list1: {
+    items: [
+      facebook,
+    ],
+    type: 'image',
+  },
+  list2: {
+    items: [
+      linkedin,
+    ],
+    type: 'image',
+  },
+  list3: {
+    items: [
+      youtube,
+    ],
+    type: 'image',
+  },
+  list4: {
+    items: [
+      contact,
+    ],
+    type: 'image',
+  },
+}
+
+export const footerData = {
+  list1: {
+    items: [
+      '1st list',
+      'list item 1',
+      'list item 2',
+      'list item 3',
+      'list item 4',
+      'list item 5',
+      'list item 6',
+    ],
+    type: 'list',
+  },
+  list2: {
+    items: [
+      '2nd list',
+      'list item 7',
+      'list item 8',
+      'list item 9',
+      'list item 10',
+      'list item 11',
+      'list item 12',
+    ],
+    type: 'list',
+  },
+  list3: {
+    items: [
+      '3rd list',
+      'list item 13',
+      'list item 14',
+      'list item 15',
+      'list item 16',
+      'list item 17',
+      'list item 18',
+    ],
+    type: 'list',
+  },
+  list4: {
+    items: [
+      img,
+    ],
+    type: 'image',
+  },
+}
+
+export const copywrite = 'Copyright © 2018 - Powered by me theodoreme@gmail.com'
