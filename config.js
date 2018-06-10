a4508cbf93beb2060c85fc0aa9a3466ddb5d0ac2
Theodoros Itzaris <theodoreme@gmail.com>
2018-06-10 22:05:16 +0300

    server: Add credentials for the mongoDB connection
    - db: is for live database on mlab
    - db_dev: is the local db connection for development use

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // if case you host db on mlab.com the connection string 
   // should look like, mongodb://<dbuser>:<dbpassword>@ds133630.mlab.com:33630/db
-  db: 'mongodb://username:password@url:port/db',
-  db_dev: 'mongodb://localhost:27017/eshop',
+  db: 'mongodb://username:password@url:port/db', // * for live mongodb, on mlab.com
+  db_dev: 'mongodb://localhost:27017/eshop', // * for local mongodb
 };
