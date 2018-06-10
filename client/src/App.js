5fe9c0675ea7428580dad54c8ad52ca34ebc66a9
Theodoros Itzaris <theodoreme@gmail.com>
2018-06-10 21:15:30 +0300

    client: Fix some eslint errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React, { Fragment } from 'react'
-import {
-  BrowserRouter as Router,
-} from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import './App.scss'
 
