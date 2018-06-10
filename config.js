06391987bf564b4e11f34b84367cc324d7195ac9
Theodoros Itzaris <theodoreme@gmail.com>
2018-06-10 17:21:37 +0300

    server: Add config

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   // if case you host db on mlab.com the connection string 
   // should look like, mongodb://<dbuser>:<dbpassword>@ds133630.mlab.com:33630/db
   db: 'mongodb://username:password@url:port/db',
-  db_dev: 'mongodb://localhost:27017/test',
+  db_dev: 'mongodb://localhost:27017/eshop',
 };
