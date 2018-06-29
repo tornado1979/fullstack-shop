2ab7cdeadc6c3623b79e03a6948d4d0379192765
Theodoros Itzaris <theodoreme@gmail.com>
2018-06-29 13:49:48 +0300

    client: Move 'renderText' to separate folder 'components/fields'

---
+++ client/src/components/fields/fields.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { TextField } from '@material-ui/core'
+import propTypes from 'prop-types'
+
+export const renderTextField = ({
+  input,
+  label,
+  meta: { touched, error },
+  fullWidth = false,
+  ...custom
+} = {}) => {
+  return (<TextField
+    error={touched && error}
+    fullWidth={fullWidth}
+    label={label}
+    multiline={custom.multiline}
+    {...input}
+    {...custom}
+  />)
+}
+
+renderTextField.propTypes = {
+  input: propTypes.shape().isRequired,
+  label: propTypes.string.isRequired,
+  meta: propTypes.shape().isRequired,
+  custom: propTypes.shape().isRequired, //eslint-disable-line
+}
+
+
+export const renderSelectField = ({
+  input,
+  label,
+  children,
+  meta: { touched, error },
+  fullWidth = false,
+  ...custom
+} = {}) => {
+  return (
+    <TextField
+      error={touched && error}
+      fullWidth={fullWidth}
+      label={label}
+      select
+      {...input}
+      onChange={(event) => {
+        input.onChange(event.target.value)
+        }
+      }
+      {...custom}
+    >
+      children={children}
+    </TextField>
+  )
+}
