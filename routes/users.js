814e13d2d074416f87cbfe7edd24d6271f951cd4
Theodoros Itzaris <theodoreme@gmail.com>
2018-07-01 12:15:10 +0300

    server: Add passport.authenticate on '/users' route

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-var User = require('../models/user')
+var passport = require('passport')
+var passportService = require('../services/passport')
+
+// var User = require('../models/user')
 
 var authController = require('../controllers/authentication')
 
+const requireAuthentication = passport.authenticate('jwt', {session: false})
 /* POST add user */
+router.get('/', requireAuthentication, function(req, res) {
+  res.send('Hello, user!!')
+})
 router.post('/signup', authController.signUp)
 
 module.exports = router;
