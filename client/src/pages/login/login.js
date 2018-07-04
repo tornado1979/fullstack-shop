43ec2b77d6a9666dc414bffefd356969d80f5225
Theodoros Itzaris <theodoreme@gmail.com>
2018-07-04 17:41:25 +0300

    client: Add login page

--- client/src/pages/login/login.js
+++ client/src/pages/login/login.js
@@ -1,13 +1,56 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import propTypes from 'prop-types'
 
 import AuthForm from '../../container/auth'
 
-const Login = () => {
-  return (
-    <main>
-      <AuthForm />
-    </main>
-  )
+import { auth } from '../../container/auth/actionCreators/auth.actionCreators'
+import { getUser } from '../../container/auth/selectors/auth.selectors'
+
+// import config from '../../../clientConfig'
+// const server = process.env.NODE_ENV === 'development' ? config.server_dev : config.server_prod
+
+class Login extends Component {
+  componentDidMount() {
+    if (this.props.isUserLoggedIn) {
+      this.props.history.push('/checkout')
+    }
+  }
+
+  handleSubmited(values) {
+    // dispatch AUTH_START
+    this.props.login(values, () => {
+      this.props.history.push('/')
+    })
+  }
+
+  render() {
+    return (
+      <main>
+        <AuthForm
+          onSubmit={(values) => this.handleSubmited(values)}
+        />
+      </main>
+    )
+  }
 }
 
-export default Login
+Login.propTypes = {
+  history: propTypes.shape().isRequired,
+  isUserLoggedIn: propTypes.bool.isRequired,
+  login: propTypes.func.isRequired,
+}
+
+const mapStateToProps = (state) => {
+  const user = getUser(state)
+  return ({
+    isUserLoggedIn: !!Object.keys(user).length,
+  })
+}
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+  login: (credentials, redirection) => auth(credentials, redirection),
+}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
