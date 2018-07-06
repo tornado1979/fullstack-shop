c54959301afcef70f4eb1fcb85f4b66b75915c9b
Theodoros Itzaris <theodoreme@gmail.com>
2018-07-06 19:14:52 +0300

    client: Show snackbar when user signup

--- client/src/middlewares/snackbar.js
+++ client/src/middlewares/snackbar.js
@@ -4,6 +4,10 @@ import {
 } from '../container/cart/actions/cart.action'
 
 import {
+  SIGNUP_FAIL,
+  SIGNUP_SUCCESS,
+} from '../container/auth/actions/auth.actions'
+import {
   showSnackBar,
   hideSnackBar,
 } from '../components/snackbar/actionCreators/snackbar.actionCreators'
@@ -11,7 +15,13 @@ import {
 // RECEIVE_PRODUCTS_SUCCESS
 export const handleSnackBar = (store) => {
   return next => action => {
+    const payload = Object.assign({}, action.payload)
+
     switch (action.type) {
+      case SIGNUP_SUCCESS:
+      case SIGNUP_FAIL:
+        store.dispatch(showSnackBar({ message: payload.message, open: true }))
+        return next(action)
       case ADD_ITEM_TO_CART:
         store.dispatch(showSnackBar({ message: 'Product added into cart', open: true }))
         return next(action)
